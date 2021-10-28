0a839190babbec0ec54c710e64a2312a34efa269
Pitt Wu <kgb00128@gmail.com>
2021-10-28 14:09:03 +0800

    feat: init

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="wrapper">
+      <Head>
+        <title>Portfolio App</title>
+        <meta name="description" content="Pitt Wu's Portfolio App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <section className="home">123</section>
+    </div>
+  );
+}
